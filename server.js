b3122a556196eefd0efd79ccb4088c7abf98b26b
rutvik31 <rutvikpandit12@gmail.com>
2021-11-16 14:51:07 +0530

    deploy5

--- server.js
+++ server.js
@@ -17,4 +17,6 @@ const userRouter = require('./route/users')
 //Router Middleware
 app.use('/users', userRouter)
 
-app.listen(3000, () => console.log('Server Started'))
+var port = process.env.PORT || 3000;
+
+app.listen(port, () =>   console.log('Server is listening on port ' + port + '!'))
